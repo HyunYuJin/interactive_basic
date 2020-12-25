c30f4f9e6332345bf2fc30db8f34537c4c39621a
현유진 <gusdbwls9772@gmail.com>
2020-12-25 23:24:23 +0900

    Complete CSS

---
+++ 섹션11_종합예제_전진_3D_스크롤/js/yj_wall3d.js
@@ -0,0 +1,7 @@
+(function() {
+    const houseElem = document.querySelector('.house');
+
+    window.addEventListener('scroll', function(e) {
+        console.log(pageYOffset); // 얼만큼 scroll을 했는지!
+    })
+})();
