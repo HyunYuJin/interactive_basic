595ec40173b02af94535912e2749c3fa9be6b045
Hyeon Yujin <gusdbwls9772@gmail.com>
2020-12-26 21:59:48 +0900

    Add running class for running characters and remove running class for stop running

---
+++ 섹션11_종합예제_전진_3D_스크롤/js/yj_character.js
@@ -0,0 +1,57 @@
+function Character(info) {
+  this.mainElem = document.createElement('div');
+  this.mainElem.classList.add('character');
+  this.mainElem.innerHTML = ''
+  + '<div class="character-face-con character-head">'
+    + '<div class="character-face character-head-face face-front"></div>'
+    + '<div class="character-face character-head-face face-back"></div>'
+    + '</div>'
+    + '<div class="character-face-con character-torso">'
+      + '<div class="character-face character-torso-face face-front"></div>'
+      + '<div class="character-face character-torso-face face-back"></div>'
+    + '</div>'
+    + '<div class="character-face-con character-arm character-arm-right">'
+      + '<div class="character-face character-arm-face face-front"></div>'
+      + '<div class="character-face character-arm-face face-back"></div>'
+    + '</div>'
+    + '<div class="character-face-con character-arm character-arm-left">'
+      + '<div class="character-face character-arm-face face-front"></div>'
+      + '<div class="character-face character-arm-face face-back"></div>'
+    + '</div>'
+    + '<div class="character-face-con character-leg character-leg-right">'
+      + '<div class="character-face character-leg-face face-front"></div>'
+      + '<div class="character-face character-leg-face face-back"></div>'
+    + '</div>'
+    + '<div class="character-face-con character-leg character-leg-left">'
+      + '<div class="character-face character-leg-face face-front"></div>'
+      + '<div class="character-face character-leg-face face-back"></div>'
+    + '</div>'
+  +'</div>';
+
+  document.querySelector('.stage').appendChild(this.mainElem);
+  this.mainElem.style.left = info.xPos + '%';
+  // 스크롤 중인지 아닌지
+  this.scrollState = false;
+  this.init();
+}
+
+// 얼굴, 몸, 팔, 다리 모두에게 공통적으로 주어야 하는 동작을 정의
+Character.prototype = {
+  constructor: Character,
+  init: function() {
+    const self = this;
+
+    window.addEventListener('scroll', function() {
+      clearTimeout(self.scrollState);
+
+      if (!self.scrollState) {
+          self.mainElem.classList.add('running');
+      }
+
+      self.scrollState = setTimeout(function() {
+        self.scrollState = false;
+        self.mainElem.classList.remove('running');
+      }, 500);
+    });
+  }
+}
--- 섹션11_종합예제_전진_3D_스크롤/js/yj_wall3d.js
+++ 섹션11_종합예제_전진_3D_스크롤/js/yj_wall3d.js
@@ -1,10 +1,39 @@
 (function() {
+    const stageElem = document.querySelector('.stage');
     const houseElem = document.querySelector('.house');
-    const maxScrollValue = document.body.offsetHeight - window.innerHeight;
+    const barElem = document.querySelector('.progress-bar');
+    const mousePos = { x: 0, y: 0 };
+    let maxScrollValue;
+
+    function resizeHandler() {
+       maxScrollValue = document.body.offsetHeight - window.innerHeight;
+    }
 
     window.addEventListener('scroll', function(e) {
         // console.log(pageYOffset); // scroll을 얼마나 했는지!
-        const zMove = pageYOffset / maxScrollValue * 980 - 490;
+        const scrollPer = pageYOffset / maxScrollValue;
+        const zMove = scrollPer * 980 - 490;
         houseElem.style.transform = 'translateZ(' + zMove + 'vw)';
-    })
+
+        // progress bar
+        barElem.style.width = scrollPer * 100 + '%';
+    });
+
+    window.addEventListener('mousemove', function(e) {
+      // console.log(e.clientX, e.clientY);
+      // console.log(window.innerWidth, window.innerHeight);
+      // console.log(mousePos);
+      mousePos.x = -1 + (e.clientX / window.innerWidth) * 2;
+      mousePos.y = 1 - (e.clientY / window.innerHeight) * 2;
+      stageElem.style.transform = 'rotateX(' + (mousePos.y * 5) + 'deg) rotateY(' + (mousePos.x * 5) + 'deg)';
+    });
+
+    stageElem.addEventListener('click', function(e) {
+      new Character({
+        xPos: e.clientX / window.innerWidth * 100
+      });
+    });
+
+    window.addEventListener('resize', resizeHandler);
+    resizeHandler(); // 처음에 문서 로드가 될 때 최초 실행을 한다.
 })();
