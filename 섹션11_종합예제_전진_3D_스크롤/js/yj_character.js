9280de20b5706f7a994cd123480661b42778e2a7
Hyeon Yujin <gusdbwls9772@gmail.com>
2020-12-26 23:41:54 +0900

    Add run function for moving lefy or right characters

--- 섹션11_종합예제_전진_3D_스크롤/js/yj_character.js
+++ 섹션11_종합예제_전진_3D_스크롤/js/yj_character.js
@@ -32,6 +32,10 @@ function Character(info) {
   this.mainElem.style.left = info.xPos + '%';
   // 스크롤 중인지 아닌지
   this.scrollState = false;
+  this.lastScrollTop = 0;
+  this.xPos = info.xPos;
+  this.speed =  0.3;
+  this.direction;
   this.init();
 }
 
@@ -52,6 +56,82 @@ Character.prototype = {
         self.scrollState = false;
         self.mainElem.classList.remove('running');
       }, 500);
+
+
+      // 이전 스크롤 위치와 현재 스크롤 위치 비교
+      if (self.lastScrollTop > pageYOffset) {
+        // 이전 스크롤 위치가 더 큰 경우(스크롤 올림)
+        self.mainElem.setAttribute('data-direction', 'backward');
+      } else {
+        // 현재 스크롤 위치가 더 큰 경우(스크롤 내림)
+        self.mainElem.setAttribute('data-direction', 'forward');
+      }
+
+      self.lastScrollTop = pageYOffset;
+    });
+
+    window.addEventListener('keydown', function(e) {
+      if (e.keyCode == 37) {
+        // 왼쪽
+        self.direction = 'left';
+        self.mainElem.setAttribute('data-direction', 'left');
+        self.mainElem.classList.add('running');
+        self.run(self);
+        // self.run();
+      } else if (e.keyCode == 39) {
+        // 오른쪽
+        self.direction = 'right';
+        self.mainElem.setAttribute('data-direction', 'right');
+        self.mainElem.classList.add('running');
+        self.run(self);
+        // self.run();
+      }
+    });
+
+    window.addEventListener('keyup', function(e) {
+      self.mainElem.classList.remove('running');
+    });
+  },
+
+  run: function(self) {
+    if (self.direction == 'left') {
+      self.xPos -= self.speed;
+    } else if (self.direction == 'right') {
+      self.xPos += self.speed;
+    }
+
+    if (self.xPos < 2) {
+      self.xPos = 2;
+    }
+
+    if (self.xPos > 88) {
+      self.xPos = 88;
+    }
+
+    self.mainElem.style.left = self.xPos + '%';
+    requestAnimationFrame(function() {
+      self.run(self);
     });
   }
+
+  // run: function() {
+  //   const self = this;
+  //
+  //   if (self.direction == 'left') {
+  //     self.xPos -= self.speed;
+  //   } else if (self.direction == 'right') {
+  //     self.xPos += self.speed;
+  //   }
+  //
+  //   if (self.xPos < 2) {
+  //     self.xPos = 2;
+  //   }
+  //
+  //   if (self.xPos > 88) {
+  //     self.xPos = 88;
+  //   }
+  //
+  //   self.mainElem.style.left = self.xPos + '%';
+  //   requestAnimationFrame(self.run.bind(self));
+  // }
 }
