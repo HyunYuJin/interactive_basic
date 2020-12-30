5bd10783bdf2b0652d572f9632ca3f958671db97
현유진 <gusdbwls9772@gmail.com>
2020-12-30 11:53:16 +0900

    Section11 practice start

---
+++ 섹션11_종합예제_전진_3D_스크롤/js/yj_practice_wall3d.js
@@ -0,0 +1,8 @@
+(function() {
+  const stageElem = document.querySelector('.stage');
+  const houseElem = document.querySelector('.house');
+
+  window.addEventListener('scroll', function() {
+    
+  });
+})();
