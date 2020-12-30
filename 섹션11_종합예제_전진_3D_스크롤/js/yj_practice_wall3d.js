d668c801e67460c061f43950904463be0d0d6014
Hyeon Yujin <gusdbwls9772@gmail.com>
2020-12-30 21:59:24 +0900

    Finished creating characters.

--- 섹션11_종합예제_전진_3D_스크롤/js/yj_practice_wall3d.js
+++ 섹션11_종합예제_전진_3D_스크롤/js/yj_practice_wall3d.js
@@ -1,8 +1,40 @@
 (function() {
+  // 마우스 움직임에 따라 .stage 꿀렁꿀렁
   const stageElem = document.querySelector('.stage');
+  // .house 내에서 스크롤을 할 것이다.
   const houseElem = document.querySelector('.house');
+  let maxScrollValue; // 스크롤할 수 있는 범위 (문서 전체 사이즈 - 창의 사이즈)
+  const barElem = document.querySelector('.progress-bar');
+  let mousePos = { x: 0, y: 0 };
+
+  // resize될 때마다 다시 계산
+  function resizeHandler() {
+    maxScrollValue = document.body.offsetHeight - window.innerHeight;
+  }
 
   window.addEventListener('scroll', function() {
-    
+    // 스크롤한 수치 (pageYOffset)
+    // 스크롤한 수치를 비율로 계산
+    let scrollPer = pageYOffset / maxScrollValue;
+    const zMove = scrollPer * 970 - 490;
+    houseElem.style.transform = 'translateZ(' + zMove + 'vw)';
+
+    barElem.style.width = scrollPer * 100  + '%';
   });
+
+  window.addEventListener('mousemove', function(e) {
+    mousePos.x = -1 + (e.clientX / window.innerWidth) * 2;
+    mousePos.y = 1 - (e.clientY / window.innerHeight) * 2;
+    stageElem.style.transform = 'rotateX(' + (mousePos.y * 5) + 'deg) rotateY(' + (mousePos.x * 5) + 'deg)';
+  })
+
+  window.addEventListener('resize', resizeHandler());
+
+  stageElem.addEventListener('click', function(e) {
+    new Character({
+      xPos: e.clientX / window.innerWidth * 100
+    });
+  })
+
+  resizeHandler();
 })();
